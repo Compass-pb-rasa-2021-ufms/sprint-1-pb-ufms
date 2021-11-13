40e935ae05e1c15ec7ca7e0d3f4c540416954ced
Dezan <dezannathan@gmail.com>
2021-11-12 22:40:33 -0400

    Design ready

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+/*
+app.get("/", (req, resp) => {
+    resp.send("http://api.ipstack.com/134.201.250.15?access_key=6c7b40831c6302fc4bca244951108a2d");
+});
+
+app.listen(8080, () => {
+    return 
+});
+
+function receiveData(){
+    console.log("teste");
+    const input = document.querySelector(".form-control");
+    const ip = "134.201.250.155";
+    const key = "?access_key=6c7b40831c6302fc4bca244951108a2d"
+
+    execute.get("http://api.ipstack.com/${ip}${key}", (req, resp) => {
+        console.log("teste");
+    });
+};
+*/
