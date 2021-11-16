e3d027c6f0cd3685b2dcbadd840683f919f8f790
f-fakeus3r <lbiazom@gmail.com>
2021-11-15 23:15:43 -0300

    Simple Lyrics Search - Heroku App
    Atualização para funcionamento como aplicativo Heroku

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sprint-1-pb-ufms
+
 Primeira sprint do programa de bolsas Compass.uol para formação em chatbot Rasa.
+
+# Objetivo e Funcionalidade
+
+O objetivo do projeto é consumir uma API pública em NodeJS.
+A API escolhida foi a Lyrics.ovh. Sua funcionalidade é trazer a letra de uma música, a partir da entrada do nome do Artista e do nome da mesma.
+
+# Tecnologias utilizadas
+
+Foram utilizados:
+-HTML e CSS para o front-end;
+-Javascript para o servidor NodeJS, onde é feito o consumo da API;
+-Git para vincular ao Heroku, onde a aplicação está hospedada.
--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 
+server.use(express.static("public"));
+
 server.use(cors());
+const port = process.env.PORT || 3000;
+server.listen(port),
+  function () {
+    console.log("Server started.......");
+  };
 
-server.listen(3000);
-
-server.get("/", (req, res) => {
-  return res.send({ message: "teste" });
-});
-
-server.get("/search/:artist/:song", async (req, res) => {
+server.get("/:artist/:song", async (req, res) => {
   const { artist, song } = req.params;
   try {
     const { data } = await api.get(`${artist}/${song}`);
