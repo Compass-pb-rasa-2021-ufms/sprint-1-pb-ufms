0213bd0f69e92da72d9e68dac8644dd326b26211
f-fakeus3r <lbiazom@gmail.com>
2021-11-25 21:46:09 -0300

    Simple Lyrics Search v1.1.1
    Updates visuais:
    -Adicionado mensagens de erro adicionais para melhor experiência do usuário
    
    Updates no código:
    -Melhoria na estrutura dos arquivos;
    -Adicionado comentários relevantes;
    -Organização geral

--- api.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const api = axios.create({
-  baseURL: "https://api.lyrics.ovh/v1/",
-});
-
-module.exports = api;
--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const api = require("./api");
+//Usando Axios para fazer a comunicação com a API
+const axios = require("axios");
+const api = axios.create({
+  baseURL: "https://api.lyrics.ovh/v1/",
+});
+
+//Usando Express como servidor
 const express = require("express");
 const server = express();
+server.use(express.static("public")); //Definindo a pasta public para elementos frontend
+
+//Requisitando o CORS no servidor
 const cors = require("cors");
-
-server.use(express.static("public"));
-
 server.use(cors());
+
 const port = process.env.PORT || 3000;
 server.listen(port),
   function () {
     console.log("Server started.......");
   };
 
+//Fazendo a conexão com a API pelo método GET
 server.get("/:artist/:song", async (req, res) => {
   const { artist, song } = req.params;
   try {
